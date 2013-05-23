979abea24419fedeb5166b39f4319c265385fd65
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-22 21:42:11 -0700

    Update js

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,23 +1,26 @@
-post '/post/:post_id/vote' do |post_id|
+post '/post/:post_id/vote' do
   if session[:user_id]
+     p params
     if params[:postvote] == "Up"
-      vote = 1
+      @vote = 1
     else
-      vote = -1
+      @vote = -1
     end
-    @post_vote = PostVote.create(user_id: session[:user_id], post_id: post_id, vote: vote)
+    @post_vote = PostVote.create(user_id: session[:user_id], post_id: params[:post_id], vote: @vote)
   end
-  redirect "/post/#{post_id}"
+  content_type :json
+  {vote: @vote}.to_json
 end
 
 post '/post/:post_id/:comment_id/vote' do |post_id, comment_id|
   if session[:user_id]
     if params[:commentvote] == "Up"
-      vote = 1
+      @vote = 1
     else
-      vote = -1
+      @vote = -1
     end
-    @comment_vote = CommentVote.create(user_id: session[:user_id], comment_id: comment_id, vote: vote)
+    @comment_vote = CommentVote.create(user_id: session[:user_id], comment_id: comment_id, vote: @vote)
   end
-  redirect "/post/#{post_id}"
+  content_type :json
+  {vote: @vote}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('button[name="postvote"]').on('click', function(event) {
+    event.preventDefault();
+    var box = $(this).val();
+    $.ajax({
+      type: 'post',
+      url: $('form[name="postvoter"]').attr('action'),
+      data: ("postvote=" + box)
+      }).done(function(data) {
+        $('form[name="postvoter"]').toggle("slow");
+        var score = parseInt($('.post-title span').text());
+        $('.post-title span').text(score + data.vote);
+      });
+  });
+  $('button[name="commentvote"]').on('click', function(event) {
+    event.preventDefault();
+    var box = $(this).val();
+    var comment = $(this).attr('label');
+    $.ajax({
+      type: 'post',
+      url: $('form[name="commentvoter"]').attr('action'),
+      data: ("commentvote=" + box)
+      }).done(function(data) {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+        $('button[label="' + comment + '"]').parent().toggle("slow");
+        var score = parseInt($('button[label="' + comment + '"]').parent().siblings().text());
+        $('button[label="' + comment + '"]').parent().siblings().text(score + data.vote);
+      });
+  });
 });
