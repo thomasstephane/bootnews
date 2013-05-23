3ccfa8b38b2ffe5bf5ccd48f1fc95ac2e206f306
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-23 00:40:38 -0700

    write button

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
-10.times do |i|
-  User.create(username: "dude#{i}", password: "password", email: "#{i}@mail.com")
-end
+User.create(username: "Adam", password: "password", email: "adam.sartell@gmail.com")
+User.create(username: "Allen", password: "password", email: "allen@quantma.com")
+User.create(username: "Dexter", password: "password", email: "dvubox@gmail.com")
+User.create(username: "Ben", password: "password", email: "benjamin.b2@gmail.com")
+User.create(username: "Stephan", password: "password", email: "thomas.stephn@gmail.com")
+User.create(username: "Nate", password: "password", email: "nathanielwroblewski@gmail.com")
+User.create(username: "Ken", password: "password", email: "kenrettberg@gmail.com")
+User.create(username: "Robert", password: "password", email: "robertjoonkim@gmail.com")
+User.create(username: "Yaron", password: "password", email: "ysadka@gmail.com")
+User.create(username: "Travis", password: "password", email: "travis.lawrence12@gmail.com")
+
 
 20.times do |i|
   Post.create(title: "new post #{i}", url: "http://url.com/#{i}", post: "This is some big content for post #{i}", user_id: (1 + rand(10)))
