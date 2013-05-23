072ece5c4ff454580ac21bea29497511b3484ac0
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-23 08:57:17 -0700

    Name update and js renaming

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.container form button[name="postvote"]').on('click', function(event) {
     event.preventDefault();
-    var box = $(this).val();
+    var vote_val = $(this).val();
     $.ajax({
       type: 'post',
       url: $('form[name="postvoter"]').attr('action'),
-      data: ("postvote=" + box)
+      data: ("postvote=" + vote_val)
       }).done(function(data) {
         $('.container form[name="postvoter"]').toggle("slow");
         var score = parseInt($('.post-title span').text());
@@ -14,12 +14,12 @@ $(document).ready(function() {
   });
   $('button[name="commentvote"]').on('click', function(event) {
     event.preventDefault();
-    var box = $(this).val();
+    var vote_val = $(this).val();
     var comment = $(this).attr('label');
     $.ajax({
       type: 'post',
       url: $(this).parent().attr('action'),
-      data: ("commentvote=" + box)
+      data: ("commentvote=" + vote_val)
       }).done(function(data) {
         $('button[label="' + comment + '"]').parent().toggle("slow");
         var score = parseInt($('button[label="' + comment + '"]').parent().parent().find('span').text());
